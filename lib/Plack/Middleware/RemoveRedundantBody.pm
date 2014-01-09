d9b07d19dabc69b5c3e20f09d4104c46465b975c
Upasana <me@upasana.me>
2014-01-09 22:05:52 +0530

    Added PODs to RemoveRedundantBody.pm

--- lib/Plack/Middleware/RemoveRedundantBody.pm
+++ lib/Plack/Middleware/RemoveRedundantBody.pm
@@ -4,6 +4,7 @@ use warnings;
 use parent qw( Plack::Middleware );
 
 use Plack::Util;
+# ABSTRACT: Plack::Middleware which sets removes body for HTTP response if it's not required
 
 sub call {
     my ($self, $env) = @_;
@@ -23,3 +24,28 @@ sub call {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::RemoveRedundantBody - removes body for HTTP response if it's not required
+
+=head1 SYNOPSIS
+
+   use strict;
+   use warnings;
+
+   use Plack::Builder;
+
+   my $app = sub { ...  };
+
+   builder {
+       enable "RemoveRedundantBody";
+       $app;
+   };
+
+=head1 DESCRIPTION
+
+This module removes body in HTTP response, if it's not required.
+
+=cut
