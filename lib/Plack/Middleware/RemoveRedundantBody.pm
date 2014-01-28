dbfaab86d3d26561e23c361e27f61f22b76aa256
Upasana <me@upasana.me>
2014-01-28 23:16:06 +0530

    Added

--- lib/Plack/Middleware/RemoveRedundantBody.pm
+++ lib/Plack/Middleware/RemoveRedundantBody.pm
@@ -4,6 +4,8 @@ use warnings;
 use parent qw( Plack::Middleware );
 use Plack::Util;
 
+our $VERSION = "0.03";
+
 # ABSTRACT: Plack::Middleware which sets removes body for HTTP response if it's not required
 
 sub call {
